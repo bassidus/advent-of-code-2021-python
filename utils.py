68c963ac88535e67e378b5fcfcefe59ac1defece
bassidus <basse1978@gmail.com>
2021-12-01 20:34:23 +0100

    refactor

--- utils.py
+++ utils.py
@@ -1,18 +1,21 @@
 from requests import get
 
 
-def fetch(url, return_type):
-    result = []
+def convert_string_list_to_int_list(lst):
+    int_list = []
+    for value in lst:
+        int_list.append(int(value))
+    return int_list
 
+
+def fetch(url, return_type=str):
     with open('.cookie') as cookie:
         cookies = dict(session=cookie.read().strip())
 
     response = get(url, cookies=cookies)
-    response = response.text.split()
+    string_list = response.text.split()
 
     if return_type == int:
-        for value in response:
-            result.append(int(value))
-        return result
+        return convert_string_list_to_int_list(string_list)
 
-    return response
+    return string_list
