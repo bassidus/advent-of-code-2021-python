32ae743278713b02614be16728067efa84bc3efc
bassidus <basse1978@gmail.com>
2021-12-01 20:03:17 +0100

    clean up

--- utils.py
+++ utils.py
@@ -2,15 +2,14 @@ from requests import get
 
 
 def fetch_ints(url):
-    cookie = '.cookie'
+    result = []
 
     with open('.cookie') as cookie:
         cookies = dict(session=cookie.read().strip())
 
     response = get(url, cookies=cookies)
-    str_lst = response.text.split()
-    int_lst = []
-    for s in str_lst:
-        int_lst.append(int(s))
+    response = response.text.split()
+    for value in response:
+        result.append(int(value))
 
-    return int_lst
+    return result
